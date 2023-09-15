eb818a3c06232f97be9ac60ba37ff1a0195de919
Luiz Paschoalin <luiz.nerf@hotmail.com>
2023-09-15 14:32:56 -0300

    city filter on all page

--- scripts/state_cities.js
+++ scripts/state_cities.js
@@ -1,18 +1,35 @@
 document.addEventListener('DOMContentLoaded', () => {
     const cityTableBody = document.getElementById('cityTableBody');
-    fetch('https://servicodados.ibge.gov.br/api/v1/localidades/estados/AC/microrregioes')
+    const filterInput = document.getElementById('filterInput'); 
+
+    let cityDataList = []; 
+    fetch('https://servicodados.ibge.gov.br/api/v1/localidades/municipios')
         .then(response => response.json())
         .then(data => {
-            data.forEach(cityData => {
+            cityDataList = data; 
+            renderTable(cityDataList); 
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        });
+
+  
+    function renderTable(dataList) {
+        cityTableBody.innerHTML = ''; 
+
+        dataList.forEach(cityData => {
+          
+            if (cityData.nome.toLowerCase().includes(filterInput.value.toLowerCase())) {
                 const row = document.createElement('tr');
                 row.innerHTML = `
                     <td>${cityData.nome}</td>
                     <td>${cityData.microrregiao.mesorregiao.UF.sigla}</td>
                 `;
                 cityTableBody.appendChild(row);
-            });
-        })
-        .catch(error => {
-            console.error('Error fetching data:', error);
+            }
         });
+    } 
+    filterInput.addEventListener('input', () => {
+        renderTable(cityDataList); 
+    });
 });
